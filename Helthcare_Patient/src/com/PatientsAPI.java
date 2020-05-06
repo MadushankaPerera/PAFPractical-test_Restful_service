cd61b59189b5003151edb57d28fea915327acd62
MadushankaPerera <Supunmadushanka95@gmail.com>
2020-05-06 15:51:25 +0530

    Updated the patientAPI

--- Helthcare_Patient/src/com/PatientsAPI.java
+++ Helthcare_Patient/src/com/PatientsAPI.java
@@ -14,11 +14,6 @@ import javax.servlet.http.HttpServletResponse;
 public class PatientsAPI extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-	
-	//Patient object
-	Patient patientobj = new Patient();
-	
-	
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -39,12 +34,22 @@ public class PatientsAPI extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		//for Insert Operation
-		
-		
-		
-		
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
 	}
 
 }
