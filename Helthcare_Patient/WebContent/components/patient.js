8e8bfab58397e195a2589a42ba6f62deff493232
MadushankaPerera <Supunmadushanka95@gmail.com>
2020-05-06 20:12:42 +0530

    Update the .js file

--- Helthcare_Patient/WebContent/components/patient.js
+++ Helthcare_Patient/WebContent/components/patient.js
@@ -1,167 +1,169 @@
-$(document).ready(function() 
-{
+$(document).ready(function() {
 	$("#alertSuccess").hide();
 	$("#alertError").hide();
-	
-});	
 
-//SAVE================================
-$(document).on("click", "#btnSave", function(event) 
-{   
+});
+
+// SAVE================================
+$(document).on("click", "#btnSave", function(event) {
 	// Clear alerts---------------------
-	$("#alertSuccess").text(""); 
-	$("#alertSuccess").hide(); 
+	$("#alertSuccess").text("");
+	$("#alertSuccess").hide();
 	$("#alertError").text("");
-	$("#alertError").hide(); 
-	
+	$("#alertError").hide();
+
 	// Form validation-------------------
 	var status = validatePatientForm();
-	if (status != true)
-	{
-		
+	if (status != true) {
+
 		$("#alertError").text(status);
 		$("#alertError").show();
 		return;
-		
+
 	}
-	
+
 	// If valid------------------------
-	var type = ($("#hidPatientIDSave").val() == "") ? "POST" : "PUT"; 
-	
-	$.ajax( 
-	{
-		
+	var type = ($("#hidPatientIDSave").val() == "") ? "POST" : "PUT";
+
+	$.ajax({
+
 		url : "PatientsAPI",
 		type : type,
 		data : $("#formPatient").serialize(),
 		dataType : "text",
-		complete : function(response, status)
-		{
-			onPatientSaveComplete(response.responseText, status); 
+		complete : function(response, status) {
+			onPatientSaveComplete(response.responseText, status);
 		}
-		
-	});
-	
-});	
-	
-	function onPatientSaveComplete(response, status)
-	{
-		
-		if (status == "success")
-		{
-			
-			var resultSet = JSON.parse(response); 
-			
-			if (resultSet.status.trim() == "success")
-			{
-				
-				$("#alertSuccess").text("Successfully saved."); 
-				$("#alertSuccess").show(); 
-				
-			} else if (resultSet.status.trim() == "error")
-			{
-				
-				$("#alertError").text(resultSet.data);
-				$("#alertError").show();
-				
-			}
-			
-			
-		} else if (status == "error")
-		{
-			
-			$("#alertError").text("Error while saving.");
-			$("#alertError").show(); 
-		}else
-		{
-			$("#alertError").text("Unknown error while saving..")
-			$("#alertError").show();
-			
-		}
-		
-		$("#hidPatientIDSave").val(""); 
-		$("#formPatient")[0].reset();
-		
-		
-	}
-	
 
-
-//REMOVE
-$(document).on("click", ".btnRemove", function(event)
-{
-	$.ajax(
-	{
-		url : "PatientsAPI",
-		type : "DELETE", 
-		data : "patientID=" + $(this).data("patientid"), 
-		dataType : "text", 
-		complete : function(response, status)
-		{
-			onPatientDeleteComplete(response.responseText, status); 
-		}
-		
 	});
-				
+
 });
 
-function onPatientDeleteComplete(response, status)
-{
-	if (status == "success")
-	{
-		var resultSet = JSON.parse(response); 
-		
-		if (resultSet.status.trim() == "success") 
-		{
-			
-			$("#alertSuccess").text("Successfully deleted."); 
-			$("#alertSuccess").show(); 
-			
-			$("#divPatientsGrid").html(resultSet.data); 
-			
-		}else if (resultSet.status.trim() == "error") 
-		{
-			
+function onPatientSaveComplete(response, status) {
+
+	if (status == "success") {
+
+		var resultSet = JSON.parse(response);
+
+		if (resultSet.status.trim() == "success") {
+
+			$("#alertSuccess").text("Successfully saved.");
+			$("#alertSuccess").show();
+
+		} else if (resultSet.status.trim() == "error") {
+
 			$("#alertError").text(resultSet.data);
 			$("#alertError").show();
-			
+
 		}
-	} else if (status == "error") 
-	{
-		
+
+	} else if (status == "error") {
+
+		$("#alertError").text("Error while saving.");
+		$("#alertError").show();
+	} else {
+		$("#alertError").text("Unknown error while saving..")
+		$("#alertError").show();
+
+	}
+
+	$("#hidPatientIDSave").val("");
+	$("#formPatient")[0].reset();
+
+}
+
+// REMOVE
+$(document).on("click", ".btnRemove", function(event) {
+	$.ajax({
+		url : "PatientsAPI",
+		type : "DELETE",
+		data : "patientID=" + $(this).data("patientID"),
+		dataType : "text",
+		complete : function(response, status) {
+			onPatientDeleteComplete(response.responseText, status);
+		}
+
+	});
+
+});
+
+function onPatientDeleteComplete(response, status) {
+	if (status == "success") {
+		var resultSet = JSON.parse(response);
+
+		if (resultSet.status.trim() == "success") {
+
+			$("#alertSuccess").text("Successfully deleted.");
+			$("#alertSuccess").show();
+
+			$("#divPatientsGrid").html(resultSet.data);
+
+		} else if (resultSet.status.trim() == "error") {
+
+			$("#alertError").text(resultSet.data);
+			$("#alertError").show();
+
+		}
+	} else if (status == "error") {
+
 		$("#alertError").text("Error while deleting.");
 		$("#alertError").show();
-		
-	}else
-	{
-		
+
+	} else {
+
 		$("#alertError").text("Unknown error while deleting..");
 		$("#alertError").show();
 	}
 
+	
+
+	// CLIENT MODEL
+	function validatePatientForm() {
+		// First Name
+		if ($("#FirstName").val().trim() == "") {
+			return "Insert First Name in textbox."
+		}
+
+		// Last Name
+		if ($("#LastName").val().trim() == "") {
+			return "Insert Last Name in textbox."
+		}
+
+		// Last Name
+		if ($("#FullName").val().trim() == "") {
+			return "Insert Last Name in textbox."
+		}
+
+		// Address
+		if ($("#HomeAddress").val().trim() == "") {
+			return "Insert Home Address in textbox."
+		}
+
+		// Date of Birth
+		if ($("#DateofBirth").val().trim() == "") {
+			return "Insert Date of Birth in textbox."
+		}
+
+		// Sex
+		if ($("#Sex").val().trim() == "") {
+			return "Insert Gender in textbox."
+		}
+
+		// Tell-No
+		if ($("#Tell-No").val().trim() == "") {
+			return "Insert Tell-No in textbox."
+		}
+
+		// Age
+		if ($("#Age").val().trim() == "") {
+			return "Insert Age in textbox."
+		}
+
+		// NIC
+		if ($("#NIC").val().trim() == "") {
+			return "Insert NIC in textbox."
+		}
+
+	}
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- Helthcare_Patient/WebContent/patients.jsp
+++ Helthcare_Patient/WebContent/patients.jsp
@@ -22,17 +22,17 @@
 		<div class="row">
 			<div class="col">
 				<h2 class ="text-primary ">Patient Management Details</h2><br>
-				<form method="post" action="patients.jsp">
+				<form method="post" action="patients.jsp" id = "formPatient">
 
-					First Name: <input name="patientFirstName" type="text" class="form-control form-control-sm" placeholder="Enter your first name here"><br>
-					Last Name: <input name="patientlastName" type="text" class="form-control form-control-sm" placeholder="Enter your last name here"><br>
-					Full Name: <input name="patientfullName" type="text"class="form-control form-control-sm" placeholder="Enter your full name here"><br>
-					Home Address: <input name="patientAddress" type="text" class="form-control form-control-sm" placeholder="Enter permenet Home Address"><br>
-					Date of Birth:<input name="patintDOB" type="text" class="form-control form-control-sm" placeholder="Enter your birth day"><br>
-					Sex: <input name="patientsex" type="text" class="form-control form-control-sm" placeholder="Enter your sex: EX. Male : 'M' Female : 'F' "><br>
-					Tell-No:<input name="patientTell" type="text" class="form-control form-control-sm" placeholder="Telephone Number"><br>
-					Age: <input name="patientAge" type="text" class="form-control form-control-sm" placeholder="Age for Today"><br> 
-					NIC:<input name="patientNIC" type="text" class="form-control form-control-sm" placeholder="National Identity Card Number"><br>
+					First Name: <input name="patientFirstName"  id="FirstName" type="text" class="form-control form-control-sm" placeholder="Enter your first name here"><br>
+					Last Name: <input name="patientlastName" id="LastName" type="text" class="form-control form-control-sm" placeholder="Enter your last name here"><br>
+					Full Name: <input name="patientfullName" id="FullName" type="text"class="form-control form-control-sm" placeholder="Enter your full name here"><br>
+					Home Address: <input name="patientAddress" id="HomeAddress" type="text" class="form-control form-control-sm" placeholder="Enter permenet Home Address"><br>
+					Date of Birth:<input name="patintDOB" id="DateofBirth" type="text" class="form-control form-control-sm" placeholder="Enter your birth day"><br>
+					Sex: <input name="patientsex" id="Sex" type="text" class="form-control form-control-sm" placeholder="Enter your sex: EX. Male : 'M' Female : 'F' "><br>
+					Tell-No:<input name="patientTell" id="Tell-No" type="text" class="form-control form-control-sm" placeholder="Telephone Number"><br>
+					Age: <input name="patientAge" id="Age" type="text" class="form-control form-control-sm" placeholder="Age for Today"><br> 
+					NIC:<input name="patientNIC" id="NIC" type="text" class="form-control form-control-sm" placeholder="National Identity Card Number"><br>
 					 
 					<input name="btnSubmit" type="submit" value="Save" class = " btn btn-primary">
 				</form>
